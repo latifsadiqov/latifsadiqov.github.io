7121eeb3fa9f629b9f9d4bb25c2240eda21fd2d6
latifsadiqov <47076926+latifsadiqov@users.noreply.github.com>
2024-07-12 21:31:14 +0400

    Delete UE-WA directory

--- UE-WA/main-folder/quiz.js
+++
@@ -1,9 +0,0 @@
-function Quiz(sorular) {
-  this.sorular = sorular;
-  this.soruIndex = 0;
-  this.dogruCevabSayi = 0;
-}
-
-Quiz.prototype.soruGetir = function () {
-  return this.sorular[this.soruIndex];
-};
